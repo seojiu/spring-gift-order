b102e342b0253042aff6cb80db574074ce89cd25
seojiu <seojiu5598@gmail.com>
2024-07-24 23:28:20 +0900

    test: 상품 수정 테스트 구현

--- src/test/java/gift/ControllerTest/ProductApiControllerTest.java
+++ src/test/java/gift/ControllerTest/ProductApiControllerTest.java
@@ -16,12 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Collections;
 
 import static org.hamcrest.Matchers.hasSize;
-<<<<<<< HEAD
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-=======
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
->>>>>>> b1a003ae (test : 상품 조회 테스트 구현)
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,8 +47,7 @@ public class ProductApiControllerTest {
 
         mockMvc.perform(post("/api/products")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"name\": \"상품1\", \"price\": 1000, \"categoryId\": "
-                                + category.getId()
+                        .content("{ \"name\": \"상품1\", \"price\": 1000, \"categoryId\": " + category.getId()
                                 + ", \"options\": [ { \"name\": \"옵션1\", \"quantity\": 10 } ] }"))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").value("상품1"))
@@ -68,17 +63,7 @@ public class ProductApiControllerTest {
         categoryRepository.save(category);
 
         Option option1 = new Option("옵션1", 10);
-<<<<<<< HEAD
-        Product product1 = new Product(
-                "상품1",
-                1000,
-                "이미지URL",
-                category,
-                Collections.singletonList(option1)
-        );
-=======
         Product product1 = new Product("상품1", 1000, "이미지URL", category, Collections.singletonList(option1));
->>>>>>> b1a003ae (test : 상품 조회 테스트 구현)
         productRepository.save(product1);
 
         mockMvc.perform(get("/api/products"))
@@ -90,57 +75,4 @@ public class ProductApiControllerTest {
                 .andExpect(jsonPath("$.content[0].options[0].name").value("옵션1"))
                 .andExpect(jsonPath("$.content[0].options[0].quantity").value(10));
     }
-<<<<<<< HEAD
-
-    @Test
-    void testUpdateProduct() throws Exception {
-        Category category = new Category("카테고리1");
-        categoryRepository.save(category);
-
-        Option option = new Option("옵션1", 10);
-        Product product = new Product(
-                "상품1",
-                1000,
-                "이미지URL",
-                category,
-                Collections.singletonList(option)
-        );
-        productRepository.save(product);
-
-        mockMvc.perform(put("/api/products/" + product.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"name\": \"상품2\", \"price\": 2000, \"categoryId\": "
-                                + category.getId()
-                                + ", \"options\": [ { \"name\": \"옵션2\", \"quantity\": 20 } ] }"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("상품2"))
-                .andExpect(jsonPath("$.price").value(2000))
-                .andExpect(jsonPath("$.options", hasSize(1)))
-                .andExpect(jsonPath("$.options[0].name").value("옵션2"))
-                .andExpect(jsonPath("$.options[0].quantity").value(20));
-    }
-
-    @Test
-    void testDeleteProduct() throws Exception {
-        Category category = new Category("카테고리1");
-        categoryRepository.save(category);
-
-        Option option = new Option("옵션1", 10);
-        Product product = new Product(
-                "상품1",
-                1000,
-                "이미지URL",
-                category,
-                Collections.singletonList(option)
-        );
-        productRepository.save(product);
-
-        mockMvc.perform(delete("/api/products/" + product.getId()))
-                .andExpect(status().isNoContent());
-
-        mockMvc.perform(get("/api/products/" + product.getId()))
-                .andExpect(status().isForbidden());
-    }
-=======
->>>>>>> b1a003ae (test : 상품 조회 테스트 구현)
 }
