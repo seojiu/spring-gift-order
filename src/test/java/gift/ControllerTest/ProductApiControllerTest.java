b1a003ae69c4ebf92feef78485bdc38947b803b1
seojiu <seojiu5598@gmail.com>
2024-07-24 23:23:31 +0900

    test : 상품 조회 테스트 구현

--- src/test/java/gift/ControllerTest/ProductApiControllerTest.java
+++ src/test/java/gift/ControllerTest/ProductApiControllerTest.java
@@ -1,6 +1,8 @@
 package gift.ControllerTest;
 
 import gift.model.Category;
+import gift.model.Option;
+import gift.model.Product;
 import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -50,5 +55,24 @@ public class ProductApiControllerTest {
                 .andExpect(jsonPath("$.options", hasSize(1)))
                 .andExpect(jsonPath("$.options[0].name").value("옵션1"))
                 .andExpect(jsonPath("$.options[0].quantity").value(10));
+    }
+
+    @Test
+    void testGetAllProducts() throws Exception {
+        Category category = new Category("카테고리1");
+        categoryRepository.save(category);
+
+        Option option1 = new Option("옵션1", 10);
+        Product product1 = new Product("상품1", 1000, "이미지URL", category, Collections.singletonList(option1));
+        productRepository.save(product1);
+
+        mockMvc.perform(get("/api/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].name").value("상품1"))
+                .andExpect(jsonPath("$.content[0].price").value(1000))
+                .andExpect(jsonPath("$.content[0].options", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].options[0].name").value("옵션1"))
+                .andExpect(jsonPath("$.content[0].options[0].quantity").value(10));
     }
 }
