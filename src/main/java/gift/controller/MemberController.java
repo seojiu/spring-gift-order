0bd2cd17a93575c7120f249889af11d9f2a50bb2
seojiu <seojiu5598@gmail.com>
2024-07-28 15:00:06 +0900

    chore: import문 최적화

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,11 +1,12 @@
 package gift.controller;
 
-import gift.dto.MemberDto;
 import gift.service.MemberService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/members")
