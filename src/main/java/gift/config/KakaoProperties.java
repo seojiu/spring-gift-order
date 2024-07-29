81837a1830a2886f79f0559b870a1bd926563b16
seojiu <seojiu5598@gmail.com>
2024-07-29 17:04:49 +0900

    feat: url 추출, 탬플릿 id 상수, 불필요한 print 제거

--- src/main/java/gift/config/KakaoProperties.java
+++ src/main/java/gift/config/KakaoProperties.java
@@ -3,5 +3,5 @@ package gift.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("kakao")
-public record KakaoProperties(String clientId, String redirectUrl, String authUrl, String apiUrl) {
+public record KakaoProperties(String clientId, String redirectUrl, String authUrl, String apiUrl, String messageSendUrl) {
 }
